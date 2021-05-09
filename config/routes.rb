d457532e84f2431730f510f4101d971d711f93dc
yuya-saito-1927 <sanda.yokoyama.nanpei@gmail.com>
2021-05-09 17:12:19 +0000

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources:books
   get 'homes' => 'homes#top', as: 'top'
+  patch'books/:id' => 'books#update', as: 'update'
+  delete'books/:id' => 'books#destroy', as: 'destroy'
 end
